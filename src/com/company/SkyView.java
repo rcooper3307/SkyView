1983f3bb379478019a0bf9cc71e6fd6d21b9f4cc
rr <rr>
2019-01-16 08:39:29 -0500

    fd

--- src/com/company/SkyView.java
+++ src/com/company/SkyView.java
@@ -9,21 +9,32 @@ public class SkyView
          int viewInt = 0;
         for (int i = 0; i < intRows; i++)
         {
-
                 for (int j = 0; j < intColumns; j++)
                 {
                     if (i % 2 == 0)
                     {
                         view[i][j] = scanned[viewInt];
                     }
-                    else {
+                    else
+                        {
                             view[i][intColumns - j - 1] = scanned[viewInt];
                         }
                         viewInt++;
-
                 }
-
         }
+    }
+    public double getAverage(int startRow, int endRow, int startCol, int endCol)
+    {
+        double sum = 0;
+        for (int i = startRow; i <= endRow; i++)
+        {
+            for(int j = startCol; j <= endCol; j++)
+            {
+                sum = sum + view[i][j];
+            }
+        }
+
+
     }
 
 }
